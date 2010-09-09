ec6eb683b1c79548fab6fbe37e75fb0fd733ae71
Kevin Griffin <griffin.kev@gmail.com>
2010-09-09 09:55:11 -0400

    empty project

---
+++ Classes/PanoramicAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PanoramicAppDelegate.h
+//  Panoramic
+//
+//  Created by Kevin Griffin on 9/9/10.
+//  Copyright Chariot Solutions LLC 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanoramicAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
